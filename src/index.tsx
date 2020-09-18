9aefea34d7411f6740e1cb089e59f152f22dfd0b
Jef Curtis <jef.curtis@clio.com>
2020-09-17 22:11:11 -0700

    Remove commented code.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import "./index.scss";
 import { App } from "./app";
-// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// serviceWorker.register();
-
